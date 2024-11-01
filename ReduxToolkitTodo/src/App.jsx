c6ec0fdbcdba4a77415d7042acf88c0fbc8fb747
iblishavi <avinashkumarjha842000@gmail.com>
2024-11-01 19:03:56 +0530

    New way of state management

---
+++ ReduxToolkitTodo/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>Hii Avi ,Work Harder to get best Results</h1>
+    </>
+  )
+}
+
+export default App
